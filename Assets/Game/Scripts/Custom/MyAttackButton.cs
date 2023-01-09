381f883a61d24da6d864ad4dc692ca50d22dacae
linhdql <linhlee88999@gmail.com>
2023-01-09 22:25:09 +0700

    Add all

--- Assets/Game/Scripts/Custom/MyAttackButton.cs
+++ Assets/Game/Scripts/Custom/MyAttackButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using MoreMountains.CorgiEngine;
@@ -7,24 +8,65 @@ using UnityEngine;
 
 public class MyAttackButton : MonoBehaviour
 {
-
+    public static float AttackSkillCooldown = 0.5333f;
     private int numberHit;
     private int oldNumberHit;
     private Character mainCharacter;
+    private DateTime _startSkillTime;
+    private DateTime _startJumpAttackSkill;
+    private Coroutine _normalCoroutine;
 
     // Start is called before the first frame update
     void Start()
     {
-        numberHit = 0;
+        numberHit = 1;
         oldNumberHit = 0;
         mainCharacter = LevelManager.Current.Players[0];
+        _startSkillTime = DateTime.MinValue;
+       
     }
 
     public void ComboHit()
     {
-        numberHit = numberHit + 1 < 4 ? numberHit + 1 : 0;
-        /*mainCharacter.CharacterAnimator.*/
+        var timeSpan = DateTime.Now - _startSkillTime;
+
+        var jumpTimeSpan = DateTime.Now - _startJumpAttackSkill;
+     
+        if (jumpTimeSpan.TotalSeconds < 2f)
+        {
+            mainCharacter.CharacterAnimator.SetBool("JumpAtk", true);
+        }
+      
+        if (timeSpan.TotalSeconds < AttackSkillCooldown)
+        {   
+            return;
+        }
+        if (timeSpan.TotalSeconds > 1f)
+        {
+            numberHit = 1;
+        }
+        else
+        {
+            numberHit = numberHit < 3 ? numberHit + 1 : 1;
+        }
+
+        Debug.Log(numberHit);
+        _startSkillTime = DateTime.Now;
+        mainCharacter.CharacterAnimator.SetInteger("Atk", numberHit);
+        _normalCoroutine = StartCoroutine(IENormalizeCharacter());
+
     }
 
-    
+    private IEnumerator IENormalizeCharacter()
+    {
+        yield return new WaitForSeconds(0.5333f);
+        mainCharacter.CharacterAnimator.SetInteger("Atk", 0);
+    }
+
+    public void SetStartJumpAttackSkill()
+    {
+       _startJumpAttackSkill = DateTime.Now;
+        mainCharacter.CharacterAnimator.SetBool("JumpAtk", false);
+       
+    } 
 }
